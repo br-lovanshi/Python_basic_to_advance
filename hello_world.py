6b67c1bf02c4dc97a917def464fc8b25ebfa9ac2
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2022-12-31 23:30:32 +0530

    python_basic

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+
+Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+# print('hello')
+name = input('enter name')
+age = input("Enterter your age ");
+is_adult = input('are you is_adult')
+print(input("say goodby 2022") );
