d5084d33b2e7d26863c3b3c239d35a393d69e64d
br-lovanshi <brajeshlovanshi2001@gmail.com>
2024-05-14 22:53:53 +0530

    python basic

---
+++ basic/data_types/dataType.py
@@ -0,0 +1,27 @@
+# print("Brajesh Lovanshi")
+
+#  a = 10
+# b = 10
+# sum = a + b
+
+# variable
+num = 10;
+string = "Hello";
+# print(num , string);
+
+
+
+# casting
+x = int(10);
+y = str(10);
+z = float(10);
+boolean = bool(True)
+print(boolean)
+
+#type
+print(type(x));
+
+list = ["apple", "orange", "mango", "banana"]
+set = {"apple", "orange", "mango", "banana"}
+dict = {"name":"Brajesh", "age":23}
+print(dict)
