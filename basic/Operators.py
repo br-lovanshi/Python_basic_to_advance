c160be3114cdcae5aa5910b172ae46c3d0270c2a
Brajesh Lovanshi <brajesh.lovanshi@lendingkart.com>
2025-03-26 17:21:23 +0530

    operators

---
+++ basic/Operators.py
@@ -0,0 +1,29 @@
+num = 4
+num2 = 5
+
+print(num * num)
+print(num / num)
+print(num // num)
+print(num - num)
+print(num == num2)
+print(num == 3)
+print(num % 3)
+print(num ** 2)
+
+print("\n\n")
+
+name = "Brajesh Lovanshi"
+
+# size
+n = len(name)
+
+# replace
+print(name.replace('L', 'l'))
+print(name.replace("Brajesh", "Vicky"))
+print(name)
+print(name.replace("Br", "bR"))
+
+# find
+print(name.find('dkd'))
+print(name.find("L"))
+print('b' in name)
