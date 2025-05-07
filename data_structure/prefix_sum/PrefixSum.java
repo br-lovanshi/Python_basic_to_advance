9e46f9733bc5954a82ca36cc3e49a42558e90a41
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2025-05-07 11:34:50 +0000

    sufix sum

--- data_structure/prefix_sum/PrefixSum.java
+++ data_structure/prefix_sum/PrefixSum.java
@@ -4,6 +4,12 @@ public class PrefixSum{
     public static void main(String[] args) {
         int n = 5;
         int[] arr = {1, 2, 3, 4, 5};
+        prefixSum(arr);
+        sufixSum(arr);
+    }
+
+    public static void prefixSum(int[] arr){
+        int n = arr.length;
         int[] prefixSum = new int[n];
         prefixSum[0] = arr[0];
         for(int index = 1; index < n; index++){
@@ -12,5 +18,18 @@ public class PrefixSum{
 
         // Print the prefix sum array
         System.out.println("Prefix Sum Array: " + Arrays.toString(prefixSum)); 
+    }
+
+    public static void sufixSum(int[] arr) {
+       int n = arr.length;
+       int[] suffixSum = new int[n];
+
+       suffixSum[n - 1] = arr[n - 1];
+       for(int i = n-2; i>=0; i--){
+            suffixSum[i] = suffixSum[i + 1] + arr[i];
+       }
+
+       System.out.println("suffix Sum Array: " + Arrays.toString(suffixSum)); 
+
     }
 }
